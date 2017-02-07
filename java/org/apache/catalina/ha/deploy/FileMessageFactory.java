24878132fb6cd1d7fe7f6042602aa4cb221904a1
Paolo Rondena <p.rondena@campus.unimib.it>
2017-02-07 12:46:56 +0100

    Fix code smell for FileMessageFactory

--- java/org/apache/catalina/ha/deploy/FileMessageFactory.java
+++ java/org/apache/catalina/ha/deploy/FileMessageFactory.java
@@ -142,9 +142,8 @@ public class FileMessageFactory {
         if (log.isDebugEnabled())
             log.debug("open file " + f + " write " + openForWrite);
         if (openForWrite) {
-            if (!file.exists())
-            	if(file.createNewFile())
-            		log.debug("file succesfully created");
+            if (!file.exists() && file.createNewFile())
+            	log.debug("file succesfully created");
             out = new FileOutputStream(f);
         } else {
             size = file.length();
